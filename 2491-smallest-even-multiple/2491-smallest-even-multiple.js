51bb7f96d26b3c91aaba07affe678ec24a2efd41
Mohammed C H <128366883+mohd-ch@users.noreply.github.com>
2024-09-03 23:18:10 +0530

    Time: 59 ms (17.15%), Space: 48.7 MB (60.75%) - LeetHub

---
+++ 2491-smallest-even-multiple/2491-smallest-even-multiple.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var smallestEvenMultiple = function(n) {
+    if(n % 2 != 1){
+        return n
+    }else{
+        return n * 2
+    }
+    
+};
