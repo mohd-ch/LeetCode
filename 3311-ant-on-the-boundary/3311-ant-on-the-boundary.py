5cedbd731a56f9828a152b9b514af5513ffe55d3
Mohammed C H <128366883+mohd-ch@users.noreply.github.com>
2024-09-07 12:11:55 +0530

    Time: 37 ms (89.05%), Space: 16.5 MB (65.77%) - LeetHub

---
+++ 3311-ant-on-the-boundary/3311-ant-on-the-boundary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def returnToBoundaryCount(self, nums: List[int]) -> int:
+        pos = 0
+        count = 0
+        for num in nums:
+            pos = pos + num
+            if(pos == 0):
+                count += 1
+        return count
+        
