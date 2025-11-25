f06588ca54cc0634b57bdaff56b5df1775a7f83a
Mohammed C H <128366883+mohd-ch@users.noreply.github.com>
2025-11-25 13:12:04 +0530

    Time: 2692 ms (5.04%), Space: 17.8 MB (55.08%) - LeetHub

--- 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
+++ 2367-number-of-arithmetic-triplets/2367-number-of-arithmetic-triplets.py
@@ -1,21 +1,24 @@
 class Solution:
     def arithmeticTriplets(self, nums: List[int], diff: int) -> int:
-        s = set(nums)
+        # self logic
         count = 0
-        for x in nums:
-            if x + diff in s and x + 2*diff in s:
-                count += 1
+        n = len(nums)
+        for i in range(n):
+            for j in range(n):
+                for k in range(n):
+                    if i < j < k :
+                        if nums[j] - nums[i] == diff :
+                            if nums[k] - nums[j] == diff :
+                                count += 1
         return count
-        # count = 0
-        # n = len(nums)
-        # for i in range(n):
-        #     for j in range(n):
-        #         for k in range(n):
-        #             if i < j < k :
-        #                 if nums[j] - nums[i] == diff :
-        #                     if nums[k] - nums[j] == diff :
-        #                         count += 1
-        # return count
+
+        # Referred and understanded advanced
+        #  s = set(nums)
+        #     count = 0
+        #     for x in nums:
+        #         if x + diff in s and x + 2*diff in s:
+        #             count += 1
+        #     return count
 
     
     
